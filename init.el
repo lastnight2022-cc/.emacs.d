6dd584367431a6335d3575b1e9f8da5443cb8a07
William Alexander <zjtomoon@dingtalk.com>
2023-10-25 10:45:15 +0800

    add company package

--- init.el
+++ init.el
@@ -2,3 +2,15 @@
 (set-frame-font "DejaVuSansMono Nerd Font-12" nil t)
 ;; 关闭备份文件
 (setq make-backup-files nil)
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(package-selected-packages '(company)))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
