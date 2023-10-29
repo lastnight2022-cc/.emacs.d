010199dd3a3b32650314037a822854434aab1e98
lastnight2022 <zjtomoon@dingtalk.com>
2023-10-29 21:31:54 +0800

    配置国内镜像站

--- init.el
+++ init.el
@@ -1,16 +1,14 @@
+;;全局配置
+
+;;设置国内镜像站
+(setq package-archives '(
+			 ("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
+			 ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
+			 ("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
+			 ))
+
 ;; 设置nerd font 字体
-(set-frame-font "DejaVuSansMono Nerd Font-12" nil t)
+;;(set-frame-font "DejaVuSansMono Nerd Font Mono Bold Oblique-12" nil t)
 ;; 关闭备份文件
 (setq make-backup-files nil)
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages '(company)))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+
