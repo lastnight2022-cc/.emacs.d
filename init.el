bde1346666315938ac24c49cb02a6791453d31bf
William Alexander <zjtomoon@dingtalk.com>
2023-10-30 11:57:11 +0800

    配置文件分离

--- init.el
+++ init.el
@@ -1,14 +1,8 @@
 ;;全局配置
 
-;;设置国内镜像站
-(setq package-archives '(
-			 ("gnu" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/gnu/")
-			 ("melpa" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa/")
-			 ("org" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/org/")
-			 ))
-
 ;; 设置nerd font 字体
-;;(set-frame-font "DejaVuSansMono Nerd Font Mono Bold Oblique-12" nil t)
+(set-frame-font "DejaVuSansMono Nerd Font Mono-12" nil t)
 ;; 关闭备份文件
 (setq make-backup-files nil)
-
+(add-to-list 'load-path "~/.emacs.d/elisp/")
+(require 'packages-install)
