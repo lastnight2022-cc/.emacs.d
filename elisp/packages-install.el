a8734a5def65ed5108d627d37db472d3cf6e1510
William Alexander <zjtomoon@dingtalk.com>
2023-10-30 16:16:48 +0800

    add company-mode

--- elisp/packages-install.el
+++ elisp/packages-install.el
@@ -26,4 +26,24 @@
 (use-package amx)
 (use-package anzu)
 
+(use-package company
+  :config
+  (setq company-dabbrev-code-everywhere t
+		        company-dabbrev-code-modes t
+		        company-dabbrev-code-other-buffers 'all
+		        company-dabbrev-downcase nil
+		        company-dabbrev-ignore-case t
+		        company-dabbrev-other-buffers 'all
+		        company-require-match nil
+		        company-minimum-prefix-length 1
+		        company-show-numbers t
+		        company-tooltip-limit 20
+		        company-idle-delay 0
+		        company-echo-delay 0
+		        company-tooltip-offset-display 'scrollbar
+		        company-begin-commands '(self-insert-command))
+  (push '(company-semantic :with company-yasnippet) company-backends)
+  :hook
+  ((after-init . global-company-mode)))
+
 (provide 'packages-install)
