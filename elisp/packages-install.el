0452fd95c25fa26029eb06f324e05a17053a632e
William Alexander <zjtomoon@dingtalk.com>
2023-10-31 10:13:44 +0800

    add some packages

--- elisp/packages-install.el
+++ elisp/packages-install.el
@@ -28,7 +28,8 @@
 (use-package restart-emacs)
 (use-package exec-path-from-shell)
 (use-package timu-rouge-theme)
-(use-package ace-window)
+(use-package ace-window
+  :bind (("M-o" . 'ace-window)))
 (use-package amx)
 (use-package anzu)
 (use-package company
@@ -50,5 +51,30 @@
   (push '(company-semantic :with company-yasnippet) company-backends)
   :hook
   ((after-init . global-company-mode)))
-(use-package lsp-mode)
+(use-package lsp-mode
+  :init
+  (setq lsp-keymap-prefix "C-c l")
+  :hook ((lsp-mode . lsp-enable-which-key-integration))
+  :commands lsp)
+(use-package lsp-ui :commands lsp-ui-mode)
+(use-package helm-lsp :commands helm-lsp-workspace-symbol)
+(use-package dap-mode)
+(use-package flycheck
+  :hook (after-init . global-flycheck-mode))
+(use-package neotree)
+(use-package super-save)
+(use-package which-key
+  :config
+  (which-key-mode))
+(use-package smart-compile)
+(use-package crux
+  :bind (("C-a" . crux-move-beginning-of-line)
+	 ("C-c ^" . crux-top-join-line)
+	 ("C-x ," . crux-find-user-init-file)
+	 ("C-s-d" . crux-duplicate-current-line-or-region)
+	 ("C-s-k" . crux-smart-kill-line)))
+(use-package rainbow-delimiters)
+(add-hook 'foo-mode-hook #'rainbow-delimiters-mode)
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
 (provide 'packages-install)
